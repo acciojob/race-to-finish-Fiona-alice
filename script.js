f74a8bea31de0f9d4edf26cac58665bbe20e3594
Fiona-alice <120317574+Fiona-alice@users.noreply.github.com>
2023-04-24 12:02:20 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-window.promises = [];
+window.promises = [
 
 // Do not change the code above this
 // add your promises to the array `promises`
+let promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise1");
+  }, 1000);
+});
+let promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise2");
+  }, 2000);
+});
+let promise3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise3");
+  }, 3000);
+});
+let promise4 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise4");
+  }, 4000);
+});
+let promise5 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise5");
+  }, 5000);
+});
+]
+Promise.any(promises).then((result)=>{
+	document.getElementById("output").innerHTML=result;
+});
