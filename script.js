3696b8ca359b650ff36586388f91cdbba74c6ca7
Fiona-alice <120317574+Fiona-alice@users.noreply.github.com>
2023-04-24 12:04:10 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
-window.promises = [
+window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
-let promise1 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Promise1");
-  }, 1000);
-});
-let promise2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Promise2");
-  }, 2000);
-});
-let promise3 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Promise3");
-  }, 3000);
-});
-let promise4 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Promise4");
-  }, 4000);
-});
-let promise5 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("Promise5");
-  }, 5000);
-});
-]
-Promise.any(promises).then((result)=>{
-	document.getElementById("output").innerHTML=result;
-});
+let output = document.querySelector("#output");
+let random = Math.floor(Math.random()*5)+1;
+let prm1 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("fulfilled 1");
+	}, random*1000);
+})
+let prm2 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("fulfilled 2");
+	}, random*1000);
+})
+let prm3 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("fulfilled 3");
+	}, random*1000);
+})
+let prm4 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("fulfilled 4");
+	}, random*1000);
+})
+let prm5 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("fulfilled 5");
+	}, random*1000);
+})
+// alert(random);
+
+promises.push(prm1)
+promises.push(prm2)
+promises.push(prm3)
+promises.push(prm4)
+promises.push(prm5)
+
+Promise.any(promises).then((value)=>{
+	output.textContent = value ;
+})
